93bec7a760b1bf7a0fa1b8d65264e9fd718e7f10
lons <lonsnw@gmail.com>
2024-03-23 08:24:03 -0500

    Adding GET in todo.router

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', (req, res) => {
+    console.log("In GET request");
+    let queryText = 'SELECT * from "tasks"';
+
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.error(error);
+        res.sendStatus(500);
+    })
+});
+
 
 // POST
 
