f46481ff579609b9617fb5c8c847cfe30a77c137
lons <lonsnw@gmail.com>
2024-03-24 16:28:48 -0500

    Adding styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,12 +4,15 @@ import axios from 'axios';
 
 function App () {
   const [task, setTask] = useState('');
-  // const [completion, setCompletion] = useState('');
   const [toDoArray, setToDoArray] = useState([]);
 
   // creating a checkbox to mark task completion
   // reference: https://www.tutorialspoint.com/how-to-use-checkboxes-in-reactjs 
   const [completion, setCompletion] = useState(false); 
+
+  // creating table sorting options
+  // reference: https://stackoverflow.com/questions/72254047/sort-table-by-clicking-on-the-column-header
+  // const [sorting, setSorting] = useState({field: 'id', ascending: false});
   
   // TO DO: GET to call tasks
   const fetchTasks = () => {
@@ -69,29 +72,37 @@ function App () {
 
   return (
     <div>
+      <header>
       <h1>let's get stuff done</h1>
+      </header>
       {/* TO DO: form for inputting tasks */}
-      <form onSubmit={addTask}>
+      <form id="addTasks" onSubmit={addTask}>
         <label htmlFor="task-input">Task:</label>
         <input id="task-input" value={task} onChange={e => setTask(e.target.value)} />
-        <br />
         <button type="submit">Add to list</button>
       </form>
       {/* <p>
         New task: {task}
       </p> */}
       {/* TO DO: list of tasks with task completion update (checkbox?) and delete button for removing */}
-      {toDoArray.map((item) => {
-        return <table>
-          <tr key={item.id}>
-          <td><input value="test" type="checkbox" onClick={(e) => {handleChange(e); updateTask(item.id)}} /></td>
-          <td className="task">{item.task}</td> 
-          {/* Got help on how to do the button here: https://react.school/ui/button */}
-          <td><button onClick={() => deleteTask(item.id)}>Delete</button></td>
-        </tr>
+      <table className="toDoTable">
+          <thead>
+            <th>Done?</th>
+            <th>Task</th>
+            <th>Delete?</th>
+          </thead>
+          {toDoArray.map((item) => {
+            return <tbody>
+            <tr key={item.id}>
+            <td><input type="checkbox" onClick={(e) => {handleChange(e); updateTask(item.id)}} /></td>
+            <td className="task">{item.task}</td> 
+            {/* Got help on how to do the button here: https://react.school/ui/button */}
+            <td><button onClick={() => deleteTask(item.id)}>Delete</button></td>
+            </tr>
+          </tbody>
+          })
+        }
         </table>
-      })
-      }
     </div>
   );
 
